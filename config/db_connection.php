84568576b060743787c0bdcd64dfcf1401a24fbb
drezhary <drezhary@gmail.com>
2025-01-13 15:24:27 +0700

    Remove database password for security compliance

--- config/db_connection.php
+++ config/db_connection.php
@@ -2,7 +2,7 @@
 $host = 'localhost';
 $dbname = 'smkcaturglobalsc_kehadiran';
 $username = 'smkcatur_admin';
-$password = 'SMKC4turGl0b4l#314';
+$password = '';
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
